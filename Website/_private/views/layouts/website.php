83ea5cd17795bd5749991be48421e1f9ef127f97
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2021-10-06 11:34:25 +0200

    responsive navbar fixed

--- Website/_private/views/layouts/website.php
+++ Website/_private/views/layouts/website.php
@@ -7,6 +7,7 @@
     <title>Homepage</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo site_url( '/css/style.css' ) ?>" media="all">
+    <link rel="stylesheet" href="<?php echo site_url( '/css/NavbarStyle.css' ) ?>" media="all">
 	<?php if ( $this->section( 'css' ) ): ?>
 		<?php echo $this->section( 'css' ) ?>
 	<?php endif; ?>
@@ -19,11 +20,16 @@
         </div>
         <div class="item item2">
             <nav>
-                <?php if ( $this->section( 'navigation' ) ): ?>
-                    <?php echo $this->section( 'navigation' ) ?>
-                <?php else: ?>
-                    <?php echo $this->fetch( '_navigation' ) ?>
-                <?php endif ?>  
+                <div class="topnav" id="myTopnav">
+                    <?php if ( $this->section( 'navigation' ) ): ?>
+                        <?php echo $this->section( 'navigation' ) ?>
+                    <?php else: ?>
+                        <?php echo $this->fetch( '_navigation' ) ?>
+                    <?php endif ?>  
+                    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                        <i class="fa fa-bars"></i>
+                    </a>
+                </div>
             </nav>
         <div class="item item3"> 
 
@@ -54,6 +60,16 @@
         &copy; <?php echo date('Y')?>
     </footer>
 </div>
+<script>
+    function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+</script>
 <?php $this->start( 'javascript' ) ?>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
