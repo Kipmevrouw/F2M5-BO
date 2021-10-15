1f3bdff1aa726187d529c87035b7a707938afefc
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2021-10-15 13:50:25 +0200

    homepage af

--- Website/_private/views/homepage.php
+++ Website/_private/views/homepage.php
@@ -78,14 +78,21 @@
 <footer>
         <section class="contact1">
             <h1>Transformers <br> Community</h1>
-            <h3>Wij empoweren jongeren <br>om mentaal gezond te zijn.</h3>
-            
+            <h2>Wij empoweren jongeren <br>om mentaal gezond te zijn.</h2>
+            <p>Stichting Transformers Community is geregistreerd <br> 
+                bij de Kamer van Koophandel onder RSIN nummer <br> 
+                861477194.</p>
+            <a style="color: white;" href="https://8e77d2ef-beb5-426a-9d28-ff09f0d53ae6.filesusr.com/ugd/e72157_eb2c2df3400a48b2b954ff865c251d6e.pdf">Jaarverslag 2020</a>
         </section>
         <section class="contact2">
+            <p style="font-size: 14px; text-align: center;">© 2021 Transformers Community</p>
 
         </section>
         <section class="contact3">
-
+            <a href="/">Home</a><br>
+            <a href="">Over ons</a><br>
+            <a href="">Word Transformer!</a><br>
+            <a href="">Contact</a>
         </section>
 </footer>
 
